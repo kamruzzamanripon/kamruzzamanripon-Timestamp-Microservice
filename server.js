9d4370b5937a74eb66014342b0e336106348ece6
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-04-26 00:26:34 +0600

    make it better 8

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ const timestamp = dateStr => {
 }
 
 // Timestamp API endpoint
-app.get("/api/timestamp/:date?", (req, res) => {
-  res.json(timestamp(req.params.date))
-})
+// app.get("/api/timestamp/:date?", (req, res) => {
+//   res.json(timestamp(req.params.date))
+// })
 
 // app.get("/api/timestamp/:timestamp", function(req, res){
 //   let timestamp = req.params.timestamp;
@@ -61,11 +61,43 @@ app.get("/api/timestamp/:date?", (req, res) => {
 //   res.json({ unix: date.valueOf(), utc: date.toUTCString()});
 // })
 
-app.get("/api/timestamp/", (req, res)=>{
-  let date = new Date();
-  res.json({unix:date.valueOf(), utc: date.toUTCString()})
+// app.get("/api/timestamp/", (req, res)=>{
+//   let date = new Date();
+//   res.json({unix:date.valueOf(), utc: date.toUTCString()})
+// })
+
+
+let responseObject = {}
+
+app.get('/api/timestamp/:input', (request, response) => {
+  let input = request.params.input
+  
+  if(input.includes('-')){
+    /* Date String */
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }else{
+    /* Timestamp */
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  
+  
+  response.json(responseObject)
 })
 
+app.get('/api/timestamp', (request, response) => {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  response.json(responseObject)
+})
 //Ripon how are you
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
